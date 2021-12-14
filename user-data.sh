d6033ecf21eb0e4b90e64804916ca5c7f9fc18c7
RaviAlexander <64334560+RaviAlexander@users.noreply.github.com>
2021-12-14 15:15:41 -0600

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,7 +1,12 @@
 #! /bin/bash -ex
 
-Env=$1
+#Env=$1
 
 yum install -y git*
 
-yum install -y httpd*
+#yum install -y httpd*
+sudo su - << EOF
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+yum -y install terraform
+EOF
